6d38f131027160d7bc663567a570b5eb3fa6352c
embr <git@liclac.eu>
2024-10-01 14:28:01 +0100

    refactor, use bluez rather than btleplug

--- src/aranet4.rs
+++ src/aranet4.rs
@@ -37,3 +37,28 @@ impl<'a> TryFromCtx<'a, ()> for Announcement {
         ))
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_announcement() {
+        assert_eq!(
+            Announcement {
+                co2: Some(697),
+                temperature: Some(23.650000000000002),
+                pressure: Some(1005.9000000000001),
+                humidity: 51,
+                battery: 20,
+                status: 1,
+            },
+            [
+                0x21, 0x13, 0x04, 0x01, 0x00, 0x0c, 0x0f, 0x01, 0xb9, 0x02, 0xd9, 0x01, 0x4b, 0x27,
+                0x33, 0x14, 0x01, 0x3c, 0x00, 0x3c, 0x00, 0xc6,
+            ]
+            .pread(0)
+            .unwrap()
+        );
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 mod aranet4;
 
-use anyhow::{anyhow, Error, Result};
-use btleplug::api::{Central, CentralEvent, Manager};
+use anyhow::{bail, Error, Result};
+use bluez_async::{AdapterEvent, BluetoothEvent, BluetoothSession, DeviceEvent, DeviceId};
 use chrono::{DateTime, Duration, Utc};
 use clap::Parser;
 use scroll::Pread;
 use serde::Serialize;
-use std::io::Write;
+use std::{cmp::max, collections::HashMap, io::Write};
 use tokio::select;
-use tokio_stream::StreamExt;
-use tracing::{debug, instrument, trace};
+use tokio_stream::{Stream, StreamExt};
+use tracing::{debug, error, instrument};
 
 #[derive(Parser, Debug)]
 #[command(version)]
@@ -47,120 +47,168 @@ fn init_logging(args: &Args) {
         .init();
 }
 
-#[derive(Debug, Clone)]
-pub struct Datapoint<T>(DateTime<Utc>, Option<T>);
-
-impl<T> Default for Datapoint<T> {
-    fn default() -> Self {
-        Self(DateTime::default(), None)
-    }
-}
-
-impl<T> Datapoint<T> {
-    fn now(v: T) -> Self {
-        Self(Utc::now(), Some(v))
-    }
-
-    fn as_ref_current(&self, stale_after: Duration) -> Option<&T> {
-        if Utc::now() - self.0 <= stale_after {
-            self.1.as_ref()
-        } else {
-            None
-        }
-    }
-}
-
-#[derive(Debug, Clone, Default)]
-pub struct Datapoints {
-    pub aranet4: Datapoint<aranet4::Announcement>,
-}
-
-impl Datapoints {
-    fn current<'a>(&'a self, stale_after: Duration) -> Output<'a> {
-        Output {
-            aranet4: self.aranet4.as_ref_current(stale_after),
-        }
-    }
-}
-
-#[derive(Debug, Clone, Serialize, Default)]
-pub struct Output<'a> {
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub aranet4: Option<&'a aranet4::Announcement>,
-}
-
 #[tokio::main]
 async fn main() -> Result<()> {
     let args = Args::parse();
     init_logging(&args);
 
-    // Stream BTLE events.
-    let btman = btleplug::platform::Manager::new().await?;
-    let adapter = btman
-        .adapters()
-        .await?
-        .into_iter()
-        .next()
-        .ok_or_else(|| anyhow!("No adapter found"))?;
-    adapter
-        .start_scan(btleplug::api::ScanFilter::default())
-        .await?;
-    let mut events = adapter.events().await?;
+    // Connect to BlueZ over DBus.
+    let (bt_join_handle, session) = BluetoothSession::new().await?;
 
-    // Tick every -i/--interval and print the latest data.
+    // Spawn a background task that processes Bluetooth events.
+    let events = session.event_stream().await?;
+    tokio::spawn(async move { run(&args, events).await });
+
+    // Start discovery.
+    session.start_discovery().await?;
+
+    // Bail out and hopefully restart if the session goes away, for some reason.
+    bt_join_handle.await?;
+    bail!("Bluetooth Session terminated!");
+}
+
+#[derive(Debug, Default)]
+struct State {
+    aranet4: HashMap<DeviceId, (DateTime<Utc>, aranet4::Announcement)>,
+}
+
+fn print_state(args: &Args, state: &State) -> Result<()> {
+    #[derive(Debug, Default, Serialize)]
+    struct OutputAranet4 {
+        pub co2: Option<u16>,
+        pub temperature: Option<f64>,
+        pub pressure: Option<f64>,
+        pub humidity: u8,
+    }
+    #[derive(Debug, Default, Serialize)]
+    struct Output {
+        #[serde(skip_serializing_if = "Option::is_none")]
+        aranet4: Option<OutputAranet4>,
+    }
+
+    // Accumulate output data.
+    let now = Utc::now();
+    let stale = Duration::from_std(std::time::Duration::from_secs_f64(args.stale)).unwrap();
+    let output = Output {
+        aranet4: state
+            .aranet4
+            .iter()
+            .filter(|(_, (ts, _))| now - ts < stale)
+            .map(|(_, (_, ann))| ann)
+            .fold(None, |out_, ann| {
+                let mut out = out_.unwrap_or_default();
+                // Use the highest currently observable readings.
+                if let Some(co2) = ann.co2 {
+                    out.co2 = Some(max(out.co2.unwrap_or_default(), co2));
+                }
+                if let Some(temp) = ann.temperature {
+                    out.temperature = Some(f64::max(out.temperature.unwrap_or_default(), temp));
+                }
+                if let Some(press) = ann.pressure {
+                    out.pressure = Some(f64::max(out.pressure.unwrap_or_default(), press));
+                }
+                out.humidity = max(out.humidity, ann.humidity);
+                Some(out)
+            }),
+    };
+
+    // Write to stdout.
+    let mut stdout = std::io::stdout();
+    serde_json::to_writer(&mut stdout, &output)?;
+    write!(&mut stdout, "\n")?;
+    stdout.flush()?;
+
+    Ok(())
+}
+
+#[instrument(skip_all)]
+async fn run(args: &Args, mut events: impl Stream<Item = BluetoothEvent> + Unpin) {
+    let mut state = State::default();
     let mut output_ticker =
         tokio::time::interval(std::time::Duration::from_secs_f64(args.interval));
-
-    // Datapoints kept.
-    let stale_after = Duration::from_std(std::time::Duration::from_secs_f64(args.stale))?;
-    let mut datapoints = Datapoints::default();
     loop {
         enum Poll {
             OutputTick,
-            CentralEvent(CentralEvent),
+            Event(BluetoothEvent),
         }
         match select! {
             _ = output_ticker.tick() => Poll::OutputTick,
-            Some(event) = events.next() => Poll::CentralEvent(event),
+            Some(event) = events.next() => Poll::Event(event),
         } {
             Poll::OutputTick => {
-                let mut stdout = std::io::stdout();
-                serde_json::to_writer(&mut stdout, &datapoints.current(stale_after))?;
-                write!(&mut stdout, "\n")?;
-                stdout.flush()?;
-            }
-            Poll::CentralEvent(event) => match event {
-                CentralEvent::ManufacturerDataAdvertisement {
-                    id,
-                    manufacturer_data,
-                } => {
-                    for (key, raw) in manufacturer_data {
-                        match key {
-                            aranet4::MANUFACTURER_ID => {
-                                let ann: aranet4::Announcement = raw.pread(0)?;
-                                debug!(
-                                    id = format!("{}", id),
-                                    raw = hex::encode(&raw),
-                                    co2 = ann.co2,
-                                    temperature = ann.temperature,
-                                    pressure = ann.pressure,
-                                    humidity = ann.humidity,
-                                    battery = ann.battery,
-                                    status = ann.status,
-                                    "📡 Aranet4 Announcement"
-                                );
-                                datapoints.aranet4 = Datapoint::now(ann);
-                            }
-                            _ => trace!(
-                                id = format!("{}", id),
-                                key = format!("{:x}", key),
-                                "❔ Unknown Manufacturer Data"
-                            ),
-                        }
-                    }
+                if let Err(err) = print_state(&args, &state) {
+                    error!(?err, "Couldn't print state");
                 }
-                _ => {}
-            },
+            }
+            Poll::Event(event) => {
+                if let Err(err) = process_event(&mut state, &event).await {
+                    error!(?event, ?err, "Event Error");
+                };
+            }
         }
     }
+}
+
+#[instrument(skip_all)]
+async fn process_event(state: &mut State, event: &BluetoothEvent) -> Result<()> {
+    match event {
+        BluetoothEvent::Adapter { id, event } => match event {
+            AdapterEvent::Powered { powered } => {
+                debug!(adp = format!("{}", id), powered, "🔌 Adapter (Un)Powered")
+            }
+            AdapterEvent::Discovering { discovering } => {
+                debug!(
+                    adp = format!("{}", id),
+                    discovering, "🔍 Adapter Discovery Status"
+                )
+            }
+            _ => {}
+        },
+        BluetoothEvent::Device { id, event } => match event {
+            DeviceEvent::ManufacturerData { manufacturer_data } => {
+                for (key, value) in manufacturer_data {
+                    match *key {
+                        aranet4::MANUFACTURER_ID => {
+                            let ann = value.pread::<aranet4::Announcement>(0)?;
+                            debug!(
+                                dev = format!("{}", id),
+                                co2 = ann.co2,
+                                temp = ann.temperature,
+                                press = ann.pressure,
+                                humid = ann.humidity,
+                                bat = ann.battery,
+                                status = ann.status,
+                                "🌬️ Aranet4 announcement"
+                            );
+                            state.aranet4.insert(id.clone(), (Utc::now(), ann));
+                        }
+                        0x004C => debug!(
+                            dev = format!("{}", id),
+                            value = hex::encode(value),
+                            "🍏 Apple Data"
+                        ),
+                        _ => debug!(
+                            dev = format!("{}", id),
+                            key = format!("{:04X}", key),
+                            value = hex::encode(value),
+                            "🔢 Manufacturer Data"
+                        ),
+                    }
+                }
+            }
+            DeviceEvent::ServiceData { service_data } => {
+                for (svc, value) in service_data {
+                    debug!(
+                        dev = format!("{}", id),
+                        svc = format!("{}", svc),
+                        value = hex::encode(value),
+                        "⚙️ Service Data"
+                    );
+                }
+            }
+            _ => {}
+        },
+        _ => {}
+    }
+    Ok(())
 }
